8b62836f61d8b4dc3be869ba4dc22c5ccde81696
fryd <fryderyk@vega.xyz>
2023-06-03 22:57:39 +0200

    chore: add service init-db command

---
+++ cmd/service/init_db.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/vegaprotocol/data-metrics-store/config"
+	"github.com/vegaprotocol/data-metrics-store/sqlstore"
+)
+
+type InitDBArgs struct {
+	*ServiceArgs
+}
+
+var initDBArgs InitDBArgs
+
+// initDBCmd represents the initDB command
+var initDBCmd = &cobra.Command{
+	Use:   "init-db",
+	Short: "Initiase monitoring schema in existing Data Node database",
+	Long:  `Initiase monitoring schema in existing Data Node database`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := RunInitDB(initDBArgs); err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	ServiceCmd.AddCommand(initDBCmd)
+	initDBArgs.ServiceArgs = &serviceArgs
+}
+
+func RunInitDB(args InitDBArgs) error {
+	cfg, logger, err := config.GetConfigAndLogger(args.ConfigFilePath, args.Debug)
+	if err != nil {
+		return err
+	}
+
+	if err := sqlstore.MigrateToLatestSchema(logger, cfg.GetConnectionConfig()); err != nil {
+		return err
+	}
+
+	return nil
+}
