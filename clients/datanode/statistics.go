83d61b90c28566bb8af39e663edda6b5df8bb489
daniel1302 <daniel.1302@gmail.com>
2024-04-05 18:17:44 +0200

    feat: add checker for healthy data node into data node api client

--- clients/datanode/statistics.go
+++ clients/datanode/statistics.go
@@ -5,10 +5,58 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+	"time"
 
 	api "code.vegaprotocol.io/protos/vega/api/v1"
 )
+
+const DataNodeHeightHeader = "x-block-height"
+
+var (
+	ErrHttpCallError            = errors.New("error during http call")
+	ErrMissingOrInvalidResponse = errors.New("missing some information from the data-node response")
+	ErrTimeGapTooBig            = errors.New("time gap between core and data-node is too big")
+	ErrBlocksGapTooBig          = errors.New("blocks gap between core and data-node is too big")
+)
+
+func (c *DataNodeClient) IsHealthy(ctx context.Context) (bool, error) {
+	payload, headers, err := c.GetStatisticsWithHeaders(ctx, []string{DataNodeHeightHeader})
+	if err != nil {
+		return false, errors.Join(ErrHttpCallError, err)
+	}
+
+	if len(headers[DataNodeHeightHeader]) < 1 {
+		return false, errors.Join(ErrMissingOrInvalidResponse, fmt.Errorf("missing %s header in the response", DataNodeHeightHeader))
+	}
+
+	currentTime, err := time.Parse(time.RFC3339, payload.CurrentTime)
+	if err != nil {
+		return false, errors.Join(ErrMissingOrInvalidResponse, fmt.Errorf("failed to parse currentTime %s: %w", payload.CurrentTime, err))
+	}
+	vegaTime, err := time.Parse(time.RFC3339, payload.VegaTime)
+	if err != nil {
+		return false, errors.Join(ErrMissingOrInvalidResponse, fmt.Errorf("failed to parse vegaTime %s: %w", payload.VegaTime, err))
+	}
+
+	strDataNodeBlockHeight := headers["x-block-height"]
+	dataNodeBlockHeight, err := strconv.ParseUint(strDataNodeBlockHeight, 10, 64)
+	if err != nil {
+		return false, errors.Join(ErrMissingOrInvalidResponse, fmt.Errorf("failed to parse x-block-height response header %s: %w", strDataNodeBlockHeight, err))
+	}
+
+	if math.Abs(float64(payload.BlockHeight-dataNodeBlockHeight)) > 150 {
+		return false, errors.Join(ErrBlocksGapTooBig, fmt.Errorf("gap between data node and core is bigger than 150 blocks"))
+	}
+
+	if currentTime.Sub(vegaTime) > time.Second*120 {
+		return false, errors.Join(ErrTimeGapTooBig, fmt.Errorf("gap between data node and core is bigger than 120 seconds"))
+	}
+
+	return true, nil
+}
 
 func (c *DataNodeClient) GetStatistics(ctx context.Context) (*api.Statistics, error) {
 	resp, _, err := c.GetStatisticsWithHeaders(ctx, nil)
